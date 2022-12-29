6e00f78dbef3739f1a51ab2952f489dbc418316a
Nikhil <nikhil.jaiswal.0802@gmail.com>
2022-12-29 11:10:16 +0530

    .

---
+++ Encapsulation/Encapsulation/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+
+    class A
+    {
+        private int age;
+        private string name;
+
+        public void setAge(int age )
+        {
+            if(age > 0 && age!=0)
+            {
+                this.age = age;
+            }
+        }
+
+        public void getAge()
+        {
+            if(age > 0 && age != 0)
+            {
+                Console.WriteLine($"Your age = {this.age}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Age");
+            }
+        }
+
+        public void setName(string Name)
+        {
+            if (string.IsNullOrEmpty(Name))
+            {
+                Console.WriteLine("Name is Required");
+            }
+            else
+            {
+                this.name = Name; 
+            }
+        }
+
+        public void getName()
+        {
+            if(!string.IsNullOrEmpty(name))
+            Console.WriteLine($"Your Name is {name}");
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new A();
+            a.setAge(0);
+            a.getAge();
+            a.setName("");
+            a.getName();
+        }
+    }
+}
