09f5cc17489cb2749e5531b1dfcee07c8ae1ef3c
Nikhil <nikhil.jaiswal.0802@gmail.com>
2022-12-29 11:14:18 +0530

    .

---
+++ Stopwatch/watch/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace watch
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("\n\n\t\t---------------------------------------------------");
+            Console.WriteLine("\t\t|                   Stopwatch                     |");
+            Console.WriteLine("\t\t---------------------------------------------------");
+            Console.Write("\n\n\t\tPress \"Backspace\" Key to start or \"Home\" Key  to exist  ");
+            var button = Console.ReadKey().Key;
+           
+
+
+            while (button  != ConsoleKey.Home)
+            {
+               
+                if (button == ConsoleKey.Backspace)
+                {
+                    var obj = StopWatch.Start<TimeSpan>();
+                    Console.Clear();
+                    Console.WriteLine("\n\n\t\t Time Starts Now .........");
+                    Console.Write("\n\n\t\t press \"Enter\" Key to stop ");
+
+                    button = Console.ReadKey().Key;
+
+                    if(button == ConsoleKey.Enter)
+                    {
+                        var obj1 = StopWatch.Stop<TimeSpan>();
+
+                        Console.Clear();
+                        
+                        StopWatch.CalculateDuration(obj, obj1);
+                        Console.Write("\n\n\t\t Press \"Backspace\" Key to Restart of \"Home\" to End  ");
+                        button = Console.ReadKey().Key;
+  
+                    }
+                }      
+            }           
+        }
+    }
+}
---
+++ Stopwatch/watch/StopWatch.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace watch
+{
+    class StopWatch
+    {
+         
+
+        public static TimeSpan Start<T>() where T : new()
+        {
+            var Hours = DateTime.Now.Hour;
+            var Minute = DateTime.Now.Minute;
+            var Seconds = DateTime.Now.Second;
+
+            TimeSpan timespan = new TimeSpan(Hours, Minute, Seconds);
+            return timespan;
+        }
+
+        public static TimeSpan Stop<T>() where T : new()
+        {
+
+            var Hours = DateTime.Now.Hour;
+            var Minute = DateTime.Now.Minute;
+            var Seconds = DateTime.Now.Second;
+
+            TimeSpan timespan = new TimeSpan(Hours, Minute, Seconds);
+            return timespan;
+        }
+
+        public static void  CalculateDuration(TimeSpan time1 , TimeSpan time2)
+        {
+            var Total_Hours = time2.Hours - time1.Hours;
+            var Total_Minutes = time2.Minutes - time1.Minutes;
+            var Total_Seconds = time2.Seconds - time1.Seconds;
+
+            TimeSpan time = new TimeSpan(Total_Hours, Total_Minutes, Total_Seconds);
+
+           
+            Console.WriteLine("\n\n\t\t Time : " + time);
+           
+
+        }
+
+        
+    }
+}
