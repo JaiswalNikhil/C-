700c418cbe2869be5ba09c186656e2cd4aeda013
Nikhil <nikhil.jaiswal.0802@gmail.com>
2022-12-29 11:05:35 +0530

    First Commit

---
+++ Constructor_Overloading/Constructor_Overloading/Constructor.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Constructor_Overloading
+{
+    class Constructor
+    {
+        int a, b;
+        string fname, lname;
+
+
+        //private Constructor()
+        //{
+        //    Console.WriteLine("Private Constructor");
+        //}
+
+        public Constructor()
+        {
+            Console.WriteLine("Default Constructor ");
+        }
+
+
+         // Static Constructor 
+         static Constructor()   
+         {
+
+            Console.WriteLine("Static Constructor");
+            Console.WriteLine();
+         }
+
+
+        public Constructor(int a , int b)
+        {
+            this.a = a;
+            this.b = b;
+
+            Console.WriteLine("Parameterized Constructor");
+
+            add(this.a,this.b);
+        }
+
+
+        public void add(int n1 , int n2 )
+        {
+
+            Console.WriteLine($"Sum of {n1} and {n2} is {n1+n2}");
+        }
+
+
+
+        public Constructor(string f , string l )
+        {
+            fname = f;
+            lname = l;
+
+            Console.WriteLine("Parameterized Constructor");
+            Console.WriteLine($"My name is {fname} {lname}");
+        }
+
+
+        public Constructor(Constructor obj)
+        {
+            Console.WriteLine("Copy Constructor");
+            int n1 = obj.a;
+            int n2 = obj.b;
+
+            add(n1, n2);
+           
+        }
+    }
+}
---
+++ Constructor_Overloading/Constructor_Overloading/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Overloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Constructor c = new Constructor();   // Default Constructor
+            Console.WriteLine();
+
+            Constructor c1 = new Constructor(2,3); // Parameterized Constructor
+            Console.WriteLine();
+
+            Constructor c2 = new Constructor("Nikhil","Jaiswal"); // Parameterized Constructor
+            Console.WriteLine();
+
+            Constructor c3 = new Constructor(c1);  // Copy Constructor
+        }
+    }
+}
