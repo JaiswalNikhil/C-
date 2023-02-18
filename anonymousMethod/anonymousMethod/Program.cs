cf70e57bfe75529b88dfcf975755a2731b8bdf01
JaiswalNikhil <117891817+JaiswalNikhil@users.noreply.github.com>
2023-02-18 18:53:04 +0530

    Add files via upload

---
+++ anonymousMethod/anonymousMethod/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace anonymousMethod
+{
+    class Program
+    {
+        public class Student
+        {
+            public int id { get; set; }
+            public  string name { get; set; }
+        }
+
+
+        //public static bool getdata(Student std)
+        //{
+        //   return std.id == 2;
+        //}
+
+        static void Main(string[] args)
+        {
+
+            List<Student> list = new List<Student>();
+            list.Add(new Student() { id = 1, name = "Nikhil" });
+            list.Add(new Student() { id = 2, name = "Piyush" });
+
+
+            // Predicate<Student> predicate = new Predicate<Student>(getdata);
+
+            //// using lamda Expression
+            Student s = list.Find(st => st.id == 2);
+            Console.WriteLine("name of student is {0}", s.name);
+
+
+            // using anonymous method
+            //Student s = list.Find(delegate (Student st) { return st.id == 2; });
+            //Console.WriteLine("name of student is {0}", s.name);
+
+            // using delegate
+            //Student s = list.Find(st => predicate(st));
+            //Console.WriteLine("name of student is {0}", s.name);
+
+
+
+            int count = list.Count(delegate (Student st)
+            {
+                return st.name.StartsWith("P");
+            });
+
+            Console.WriteLine(count);
+
+
+            //Func<Student, string> func = st1 => "Name = " st1.name;
+
+
+            IEnumerable<string> names = list.Select((Student st1) => "Name = " + st1.name);
+
+            foreach (var n in names)
+            {
+                Console.WriteLine(n);
+            }
+
+            Func<int, int, int, int> func = (n1, n2, n3) => n1 + n2 + n3;
+            Console.WriteLine("sum = " + func(1, 2, 3));
+
+
+            //int? a = null;
+            //Console.WriteLine(a.GetValueOrDefault());
+            //a.GetValueOrDefault();
+
+            DateTime? date = new DateTime(2002,10,08);
+            DateTime date1 = date.GetValueOrDefault();
+
+            DateTime date2 = (date != null) ? date.GetValueOrDefault() : DateTime.Today;
+            Console.WriteLine(date2);
+            Console.WriteLine(date.GetValueOrDefault());
+            Console.WriteLine(date.HasValue);
+
+
+            int x = 10;
+            float y = 10.98f;
+            int z = (int)y;
+            Console.WriteLine(z);
+
+            dynamic d = y;
+
+            Console.WriteLine(d);
+
+            dynamic  e = d;
+            Console.WriteLine(e);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ array/array/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int position ,direction;
+            
+
+            int[] N = new int[6];
+
+            Console.WriteLine("Enter 6 values in array");
+            for(int i =  0; i < 6; i++)
+            {
+                N[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            foreach (var num in N)
+            {
+                Console.Write(num + " , ");
+            }
+            Console.WriteLine();
+
+            Console.Write("Enter the position(0 > p < ArraySize) : ");
+            position = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the Direction ( 0 for left and 1 for right : ");
+            direction = Convert.ToInt32(Console.ReadLine());
+
+
+
+            if(direction == 0)
+            {
+                if(position == 1)
+                {
+                    int a = N[0];
+                    N[0] = N[1];
+                    N[1] = N[2];
+                    N[2] = N[3];
+                    N[3] = N[4];
+                    N[4] = N[5];
+                    N[5] = a;
+
+                    foreach (var num in N)
+                    {
+                        Console.Write(num + " , ");
+                    }
+
+                }
+                 else if (position == 2)
+                {
+                    int a = N[0];
+                    int b = N[1];
+
+                    N[0] = N[2];
+                    N[1] = N[3];
+                    N[2] = N[4];
+                    N[3] = N[5];
+                    N[4] = a;
+                    N[5] = b;
+
+                    foreach (var num in N)
+                    {
+                        Console.Write(num + " , ");
+                    }
+
+                }
+                else if (position == 3)
+                {
+                    int a = N[0];
+                    int b = N[1];
+                    int c = N[2];
+                    N[0] = N[3];
+                    N[1] = N[4];
+                    N[2] = N[5];
+                    N[3] = a;
+                    N[4] = b;
+                    N[5] = c;
+
+                    foreach (var num in N)
+                    {
+                        Console.Write(num + " , ");
+                    }
+                }
+                else if (position == 4)
+                {
+                    int a = N[0];
+                    int b = N[1];
+                    int c = N[2];
+                    int d = N[3];
+
+                    N[0] = N[4];
+                    N[1] = N[5];
+                    N[2] = a;
+                    N[3] = b;
+                    N[4] = c;
+                    N[5] = d;
+
+                    foreach (var num in N)
+                    {
+                        Console.Write(num + " , ");
+                    }
+                }
+                else if (position == 5)
+                {
+                    int a = N[0];
+                    int b = N[1];
+                    int c = N[2];
+                    int d = N[3];
+                    int e = N[4];
+
+                    N[0] = N[5];
+                    N[1] = a;
+                    N[2] = b;
+                    N[3] = c;
+                    N[4] = d;
+                    N[5] = e;
+
+                    foreach (var num in N)
+                    {
+                        Console.Write(num + " , ");
+                    }
+
+                }
+                
+            }
+            
+
+        }
+    }
+}
